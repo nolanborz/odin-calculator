d7470ac8fcd293835b1a2b3b1519ba3e82dd88fb
nolanborzoni <nolanrborzoni@gmail.com>
2024-05-27 22:38:16 +0900

    Add button queries and functionality
    Added the button dom select queries and connected
    one so that, when pressed, it will show up in the
    display and begin building a string.

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 //initiate selectors
-const additionButton = document.getElementById("add");
+const numberDisplay = document.getElementById('numberDisplay');
 const allButtons = document.querySelectorAll("button");
+
+const addButton = document.getElementById("add");
+const subButton = document.getElementById('minus');
+const multButton = document.getElementById('multiply');
+const divButton = document.getElementById('divide');
+const equalButton = document.getElementById('equals');
+const clearButton = document.getElementById('clear');
+//number selectors
+const nineButton = document.getElementById('nine');
+const eightButton = document.getElementById('eight');
+const sevenButton = document.getElementById('seven');
+const sixButton = document.getElementById('six');
+const fiveButton = document.getElementById('five');
+const fourButton = document.getElementById('four');
+const threeButton = document.getElementById('three');
+const twoButton = document.getElementById('two');
+const oneButton = document.getElementById('one');
 
 //addition, subratction, multiplication, and division functions
 function add(value1, value2) {
@@ -36,18 +53,25 @@ function operate(value1, value2, operator) {
         return divide(value1, value2);
     };
 }
-operate(6, 2, '/');
+
+//clear function
+function clearDisplay() {
+    numberDisplay.textContent = '';
+}
+
 
 function buttonResponse() {
     allButtons.forEach(mouseResponseColorChange);
+    oneButton.addEventListener('click', () => {
+        
+        numberDisplay.textContent += '1'});
 }
 
 function mouseResponseColorChange(item, index, array) {
     item.addEventListener('mousedown', () => item.style.background = 'beige');
     item.addEventListener('mouseup', () => item.style.background = 'rgb(255, 234, 227)');
-    }
+}
 
 
 buttonResponse();
-//allButtons.addEventListener('mousedown', () => allButtons.style.background = 'beige');
-//additionButton.addEventListener('mouseup', () => additionButton.style.background = 'rgb(255, 234, 227)');
+clearButton.addEventListener('click', () => clearDisplay());
